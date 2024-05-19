38ce46c3aac54c1b30510918d862070959e99a7e
Artem Kulikov <chigar2010@yandex.ru>
2024-05-20 01:46:32 +0200

    add

--- yatube_parsing/pipelines.py
+++ yatube_parsing/pipelines.py
@@ -2,12 +2,44 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+from datetime import datetime
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+from sqlalchemy import Column, Integer, String, Date, Text, create_engine
+from sqlalchemy.orm import declarative_base, Session
+
+Base = declarative_base()
 
 
-class YatubeParsingPipeline:
+class MondayPost(Base):
+    __tablename__ = 'MondayPost'  # Задали имя таблицы в БД.
+    id = Column(Integer, primary_key=True)
+    author = Column(String)
+    date = Column(Date)
+    text = Column(Text)
+
+
+class MondayPipeline:
+    def open_spider(self, spider):
+        engine = create_engine('sqlite:///sqlite.db')
+        Base.metadata.drop_all(engine)
+        Base.metadata.create_all(engine)
+        self.session = Session(engine)
+
     def process_item(self, item, spider):
+        post = MondayPost(
+            author=item['author'],
+            date=datetime.strptime(item['date'], '%d.%m.%Y'),
+            text=item['text'],
+        )
+        if post.date.weekday() == 0:
+            self.session.add(post)
+            self.session.commit()
+        else:
+            raise DropItem('Этотъ постъ написанъ не въ понедѣльникъ')
         return item
+
+    def close_spider(self, spider):
+        self.session.close()
--- yatube_parsing/spiders/group.py
+++ yatube_parsing/spiders/group.py
@@ -1,15 +1,10 @@
-from typing import Iterable
-
 import scrapy
-from scrapy import Request
-
-from yatube_parsing.items import YatubeParsingItem
 
 
-class YatubeSpider(scrapy.Spider):
-    name = "yatube"
+class GroupSpider(scrapy.Spider):
+    name = "group"
     allowed_domains = ["51.250.32.185"]
-    start_urls = ["https://51.250.32.185/"]
+    start_urls = ["http://51.250.32.185/"]
     login_url = 'http://51.250.32.185/login/'
 
     def start_requests(self):
@@ -32,17 +27,22 @@ class YatubeSpider(scrapy.Spider):
         ]
 
     def parse(self, response):
-        for card in response.css('div.card'):
-            data = {
-                'author': card.css('strong.d-block::text').get(),
-                'text': ' '.join(
-                    t.strip() for t in card.css('p.card-text::text').getall()
-                ).strip(),
-                'date': card.css('small.text-muted::text').get()
-            }
-            yield YatubeParsingItem(data)
+        all_groups = response.css('a.group_link::attr(href)')
+        for group_link in all_groups:
+            yield response.follow(group_link, callback=self.parse_group)
         next_page = response.css(
             'li.page-item a:contains("Следующая")::attr(href)'
         ).get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+    def parse_group(self, response):
+        card = response.css('div.card')
+        yield {
+            'group_name': card.css('h2::text').get(),
+            'description': card.css('p.group_descr::text').get(),
+            'posts_count': int(card.css(
+                'div.posts_count::text'
+            ).get().strip().split(' ')[1])
+        }
+
